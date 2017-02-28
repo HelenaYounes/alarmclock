10f6d3fdbf08fe523383a960002968401b1c48e1
HelenaYounes <younesq.helena@gmail.com>
2017-02-28 13:56:43 -0700

    simple countdown with alarm

---
+++ alarm.js
@@ -0,0 +1,51 @@
+
+var myInterval;
+var startcount;
+var reset;
+
+
+var onLoad = function(){
+  var go = document.querySelector('.starter');
+  go.addEventListener('click',starting);
+  var stopbutton = document.querySelector('.stoper');
+  stopbutton.addEventListener('click', stoping);
+  var resetbutton = document.querySelector('.reseter');
+  resetbutton.addEventListener('click',reseting);
+
+
+
+
+}
+
+function decrement(){
+  startcount = document.querySelector('.counter');
+  if(startcount != null){
+    startcount.innerHTML--;
+  }
+  else{
+    alert("time Out!");
+  }
+  }
+
+
+
+
+
+
+function starting(){
+   myInterval = setInterval (decrement, 1000);
+}
+
+function stoping(){
+ clearInterval(myInterval);
+
+}
+
+function reseting(){
+  startcount.innerHTML = 0;
+
+}
+
+
+
+window.addEventListener('load',onLoad);
